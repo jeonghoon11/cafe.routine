da66daf2a9ad1b102e98df321db068b474fb8dc3
jeonghoon11 <jih361@naver.com>
2026-08-08 19:49:59 +0900

    chore: React 프로젝트 초기 설정

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'prettier';
+
+const config: Config = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 80,
+};
+
+export default config;
